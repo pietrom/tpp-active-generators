a7779b6422c27ba9db45ea82442348a24328aea3
Pietro Martinelli <amicofragile@gmail.com>
2012-09-11 12:54:04 +0200

    Added field accessors generation

--- scripts/MessageToJava.groovy
+++ scripts/MessageToJava.groovy
@@ -1,5 +1,41 @@
 println "Running MessageToJava ${args[0]} ${args[1]}"
 
+def createJavaClass(out, pkg, message, fields) {
+	out.delete()
+
+	println "Creating file $out..."
+
+	out << "package $pkg;\n\n"
+	out << "public class $message {\n"
+	fields.each {
+		writeFieldDefinition(out, it)		
+	}
+	
+	fields.each {
+		writeAccessorsDefinitionwrite(out, it)		
+	}
+	out << "}\n"
+
+	println "File $out created!"
+}
+
+def writeFieldDefinition(out, field) {
+	out << "\tprivate String $field;\n"
+}
+
+def writeAccessorsDefinitionwrite(out, field) {
+	def capitalizedField = field[0].toUpperCase() + (field.size() > 1 ? field[1..-1] : "")
+	out << """
+	public void set${capitalizedField}(String val) {
+		this.$field = val;
+	}
+	
+	public String get${capitalizedField}() {
+		return this.$field;
+	}
+"""
+}
+
 
 def processMessage(messageFile) {
 	def message = null
@@ -30,18 +66,8 @@ def processMessage(messageFile) {
 	outDir.mkdirs()
 	
 	def out = new File(outDir, message + ".java")
-	out.delete()
-
-	println "Creating file $out..."
-
-	out << "package $pkg;\n\n"
-	out << "public class $message {\n"
-	fields.each {
-		out << "\tprivate String $it;\n"
-	}
-	out << "}\n"
-
-	println "File $out created!"
+	
+	createJavaClass(out, pkg, message, fields)
 }
 
 new File(args[0]).eachFile {
--- src/test/java/org/amicofragile/test/MSG01Test.java
+++ src/test/java/org/amicofragile/test/MSG01Test.java
@@ -1,10 +1,18 @@
 package org.amicofragile.test; 
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class MSG01Test {
 	@Test
 	public void createMSG01() {
 		final MSG01 msg = new MSG01();
+	}
+	
+	@Test
+	public void setGetValue() {
+		final MSG01 msg = new MSG01();
+		msg.setXxx("TEST");
+		assertEquals("TEST", msg.getXxx());
 	}
 }
